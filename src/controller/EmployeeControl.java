408530ee807123c3dff45d6f860e276ec4d45873
ndc1509 <78099549+ndc1509@users.noreply.github.com>
2021-10-16 13:02:41 +0700

    2nd

--- src/controller/EmployeeControl.java
+++ src/controller/EmployeeControl.java
@@ -26,7 +26,7 @@ public class EmployeeControl {
 			view.addEmpListener(new AddEmpListener());
                         view.editEmpListener(new EditEmpListener());
                         view.updateEmpListener(new UpdateEmpListener());
-                        view.delEmpListener(new DeleteEmpListener());
+                        view.deleteEmpListener(new DeleteEmpListener());
 		} catch (ClassNotFoundException | SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -34,47 +34,7 @@ public class EmployeeControl {
 			System.exit(0);
 		}
 	}
-
-//	public void run(){
-//		int choice = -1;
-//		do {
-//			choice = view.menu();
-//			switch (choice) {
-//			case 1:
-//				emlopyees=this.displayAll();
-//				if(!emlopyees.equals(null)){
-//					for (int i = 0; i < emlopyees.length; i++) {
-//						this.view.showMessage(emlopyees[i].toString());
-//					}
-//				}
-//				break;
-//
-//			case 2:
-//				Employee e = this.view.input(2);
-//				emlopyees=this.displayByName(e.getEmpName());
-//				if(!emlopyees.equals(null)){
-//					for (int i = 0; i < emlopyees.length; i++) {
-//						this.view.showMessage(emlopyees[i].toString());
-//					}
-//				}
-//				break;
-//			case 3: 
-//				Employee insertE = this.view.input(3);
-//				int rowCout = this.dao.insert(insertE);
-//				this.view.showMessage(rowCout+ " row has been inserted");
-//				break;
-//			case 4:
-//				Employee upE = this.view.input(4);
-//				int updateCount = this.dao.update(upE);
-//				this.view.showMessage(updateCount+ " row has been inserted");
-//				break;
-//			default:
-//				break;
-//			}
-//		} while (choice!=0);
-//	}
-	
-        
+      
         class AddEmpListener implements ActionListener{
             public void actionPerformed(ActionEvent e) {
                 Employee emp = view.getEmployee();
@@ -108,8 +68,7 @@ public class EmployeeControl {
                 displayAll();
             }            
         }
-        
-        
+         
 	public void displayAll(){
             List<Employee> empList =  Arrays.asList(dao.selectAll());
             view.showListEmployee(empList);
